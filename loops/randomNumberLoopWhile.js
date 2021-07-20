55556d6b37fd83bf7bc23da8b34c9d2f16a72ce2
rafael.dyszy <rafaeldyszy@hotmail.com>
2021-07-20 19:17:52 +0200

    update

--- loops/randomNumberLoopWhile.js
+++ loops/randomNumberLoopWhile.js
@@ -3,6 +3,8 @@ function generateNumbers(max) {
 }
 
 let arr = [];
+let arrNumbers = [8, 5, 3, 0, 7];
+let numbersRight = [];
 
 while (arr.length < 5) {
   let randomNumber = generateNumbers(10);
@@ -10,5 +12,23 @@ while (arr.length < 5) {
     arr.push(randomNumber);
   }
 }
-console.log(arr.length);
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arrNumbers.length; j++) {
+    if (arr[i] == arrNumbers[j]) {
+      numbersRight.push(arr[i]);
+    }
+  }
+}
+
+if (numbersRight.length == 3) {
+  console.log(`you win`);
+} else {
+  console.log(`You lost`);
+}
+
 console.log(arr);
+console.log(arrNumbers);
+console.log(
+  `Number right are ${numbersRight} and the total ${numbersRight.length}`
+);
