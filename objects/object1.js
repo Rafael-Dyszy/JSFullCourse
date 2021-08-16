64f48403ace6fe34bb46927973f547bc224403ea
rafael.dyszy <rafaeldyszy@hotmail.com>
2021-08-16 21:04:51 +0200

    add objects lesson

---
+++ objects/object1.js
@@ -0,0 +1,20 @@
+const obj = new Object();
+obj.name = 'Rafal';
+obj.age = 36;
+obj['email'] = 'obj@obj.com';
+console.log(obj);
+
+const obj2 = {
+  name: 'Rafal Dyszy',
+  age: 36,
+};
+// console.log(obj2);
+
+const obString = new String('new string');
+// console.log(obString);
+
+const objNumber = new Number(30);
+// console.log(objNumber);
+
+const objBoolean = new Boolean('It`s true because has a value! obviously');
+console.log(objBoolean);
