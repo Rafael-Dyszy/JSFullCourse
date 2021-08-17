8c5dc45cfacf80b5113a89e213f2c7695219b3cf
rafael.dyszy <rafaeldyszy@hotmail.com>
2021-08-17 23:39:39 +0200

    final Math

--- Math/exercises/randomNumbers/exercise-random.js
+++ Math/exercises/randomNumbers/exercise-random.js
@@ -2,4 +2,3 @@ function getNumberInterger(start = 0, final = 1, integer = true) {
   let r = Math.random() * (final - start + 1) + start;
   return integer ? parseInt(r) : r;
 }
-console.log(getNumberInterger(2, 3, false));
