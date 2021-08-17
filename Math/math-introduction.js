d251aa959597299e75869d5422c163bae40d4945
rafael.dyszy <rafaeldyszy@hotmail.com>
2021-08-17 23:06:36 +0200

    add Math

---
+++ Math/math-introduction.js
@@ -0,0 +1,23 @@
+console.log(Math.max(1, 2, 10, 2, 4, 5, 8, 12, 1, 2, 3));
+console.log(Math.min(1, 2, 10, 2, 4, 5, 8, 12, 1, 2, 3));
+const numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2];
+console.log(Math.max(...numberArr));
+
+console.log(Math.round(4.49));
+console.log(Math.round(4.51));
+
+console.log(Math.floor(4.49));
+console.log(Math.ceil(4.49));
+
+// pow is 2 * 2 * 2 = 8
+console.log(Math.pow(2, 3));
+console.log(2 ** 3);
+
+console.log(Math.sqrt(49));
+console.log(Math.cbrt(8));
+
+console.log(Math.random());
+console.log(Math.random() * 1);
+console.log(Math.random() * 10);
+
+console.log(Math.floor(Math.random() * 100));
